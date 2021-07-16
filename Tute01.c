98376a8b7f73f1a7ab5b7cc9887f439e882bf699
IT21092374 <87359900+SandaliRathnayaka@users.noreply.github.com>
2021-07-16 03:41:39 +0000

    completed

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,15 @@
 #include <stdio.h>
 
 int main() {
+  int mark1,mark2;
+  float avg;
+  printf("input mark 1 = ");
+  scanf("%d",&mark1);
+  printf("input mark 2 = ");
+  scanf("%d",&mark2);
+  avg = (mark1 + mark2) / 2.0;
+  printf("average is = %.2f",avg);
+
   
   return 0;
 }
--- Tute02.c
+++ Tute02.c
@@ -20,5 +20,7 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 int main() {
   
+  
+  
   return 0;
 }
