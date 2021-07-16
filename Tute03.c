7ed831487086329f663291111a3fae86c8e8c58f
IT21092374 <87359900+IT21092374@users.noreply.github.com>
2021-07-16 07:57:18 +0000

    completed

--- Tute03.c
+++ Tute03.c
@@ -13,6 +13,18 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+  int no,count = 1,sum = 0;
+
+  printf("enter a positive number = ");
+  scanf("%d",&no);
+
+  while(count <= no)
+  {
+    sum = sum + count;
+    count++;
+
+  }
+  printf("sum is = %d\n",sum);
   
   return 0;
 }
